356e064e576bbea6233352450575c70b4b27aaa8
paolo9688 <paolo.pz1@gmail.com>
2025-06-25 10:27:27 +0200

    first commit

---
+++ src/main/java/com/example/car_crud_exercise/CarCrudExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.car_crud_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCrudExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarCrudExerciseApplication.class, args);
+	}
+
+}
